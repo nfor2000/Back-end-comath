eab54f9997b11e7e83c349df4bbd31571b847445
nfor2000 <nforpresly2@gmail.com>
2023-12-01 19:47:26 +0100

    commit

--- config/dbase.js
+++ config/dbase.js
@@ -1,10 +1,11 @@
 
 
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connectDb = async () => {
      try {
-          const conn = await mongoose.connect('mongodb://localhost:27017/webDb')
+          const conn = await mongoose.connect(process.env.MONGODB_URI)
           console.log(`Successfully connected to Mongodb`);
           
      }catch (error) {
